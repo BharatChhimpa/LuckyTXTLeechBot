b0ace98640e57b080b183bb36e5b504f46fbe824
BharatChhimpa <bhartlal2013@gmail.com>
2025-03-21 11:07:13 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 27881923
-api_hash = "79abda5e46a51fc0dce1313f2548ce19"
-bot_token = "6263666911:AAF_Zm2baKNCJMGPn8aCBlVArvTa-A4kz1k"
+api_id = 29683927
+api_hash = "b2eb32ac1030edd8bc36c7b554ef6fc3"
+bot_token = "7903348261:AAFgADxgmNQUiW9UhF_0srnTFCKWT6RLW90"
 
